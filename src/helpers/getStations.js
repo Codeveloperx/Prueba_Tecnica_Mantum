a7cc730db8ee860cb405b3d430e1024dc770a381
Camilo Orozco Hdz <camilo.orozcohdz@gmail.com>
2023-02-26 23:43:09 -0500

    pruebas

--- src/helpers/getStations.js
+++ src/helpers/getStations.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 export const getStations = async() =>{
 
-    const endPoint = 'https://myttc.ca/finch_station.json';
+    const endPoint = 'https://backpruebatecnica-production.up.railway.app/stops';
     // const endPoint = 'http://localhost:3000/stops';
 
     try{
         const resp = await axios.get( endPoint );
         const data  = await resp.data;
-        // console.log(data)
+        console.log(data)
         return data;
     }
     catch{
